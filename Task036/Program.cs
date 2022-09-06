3e3b78ee5663b0dddc6c6f776d53b6d40db8688f
Alex <al.repin.a@yandex.ru>
2022-09-06 11:45:36 +0300

    add task036

---
+++ Task036/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными
+// числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int[] SummRandom(int num)
+{
+    int[] array = new int[num];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100);
+    }
+    return array;
+}
+
+var arr = SummRandom(9);
+int sum = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+
+    if (i % 2 == 1)
+    {
+        sum += arr[i];
+    }
+}
+
+Console.WriteLine($"[{string.Join(", ", arr)}]");
+Console.WriteLine(sum);
